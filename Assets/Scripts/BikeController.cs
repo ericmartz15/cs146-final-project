23026dfb01b89410681090922d6082bd7adce36c
krwang11 <153586441+krwang11@users.noreply.github.com>
2026-03-09 21:34:29 -0700

    added coffee boosters, initial work on minimap

--- Assets/Scripts/BikeController.cs
+++ Assets/Scripts/BikeController.cs
@@ -2,6 +2,7 @@
 // Smooth top-down movement for the player bike.
 // Checks road tilemap before moving, same approach as MoveScript.
 
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -24,12 +25,16 @@ public class BikeController : MonoBehaviour
     private Vector2 movement;
     private PlayerHealth playerHealth;
 
+    private float currentSpeed;
+    private Coroutine activeBoost;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         playerHealth = GetComponent<PlayerHealth>();
         rb.freezeRotation = true;
+        currentSpeed = moveSpeed;
     }
 
     void Update()
@@ -50,42 +55,66 @@ public class BikeController : MonoBehaviour
     {
         if (roadTilemap == null)
         {
-            rb.velocity = movement * moveSpeed;
+            rb.velocity = movement * currentSpeed;
             return;
         }
 
         Vector2 edgePos = rb.position + movement.normalized * edgeOffset;
-        Vector2 nextPos = edgePos + movement * moveSpeed * Time.fixedDeltaTime;
+        Vector2 nextPos = edgePos + movement * currentSpeed * Time.fixedDeltaTime;
         Vector3Int nextCell = roadTilemap.WorldToCell(nextPos);
 
         if (roadTilemap.HasTile(nextCell))
         {
-            rb.velocity = movement * moveSpeed;
+            rb.velocity = movement * currentSpeed;
         }
         else
         {
             Vector2 moveX = new Vector2(movement.x, 0f);
             Vector2 moveY = new Vector2(0f, movement.y);
 
-            Vector3Int cellX = roadTilemap.WorldToCell(edgePos + moveX * moveSpeed * Time.fixedDeltaTime);
-            Vector3Int cellY = roadTilemap.WorldToCell(edgePos + moveY * moveSpeed * Time.fixedDeltaTime);
+            Vector3Int cellX = roadTilemap.WorldToCell(edgePos + moveX * currentSpeed * Time.fixedDeltaTime);
+            Vector3Int cellY = roadTilemap.WorldToCell(edgePos + moveY * currentSpeed * Time.fixedDeltaTime);
 
             Vector2 constrainedMovement = Vector2.zero;
             if (roadTilemap.HasTile(cellX)) constrainedMovement.x = movement.x;
             if (roadTilemap.HasTile(cellY)) constrainedMovement.y = movement.y;
 
-            rb.velocity = constrainedMovement * moveSpeed;
+            rb.velocity = constrainedMovement * currentSpeed;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("BikeController collision with: " + collision.gameObject.name);
-        if (collision.gameObject.GetComponent<MotorcycleBehavior>() != null)
+        if (collision.gameObject.GetComponent<MotorcycleBehavior>() != null ||
+            collision.gameObject.GetComponent<BusBehavior>() != null)
         {
-            Debug.Log("Hit by motorcycle - triggering death");
             if (playerHealth != null && !playerHealth.IsDead)
                 playerHealth.TakeDamage(playerHealth.MaxLives);
         }
+    }
+
+    /// <summary>
+    /// Called by CoffeePickup when the player collects a coffee.
+    /// Temporarily multiplies move speed by speedMultiplier for boostDuration seconds.
+    /// If a boost is already active, it is replaced by the new one.
+    /// </summary>
+    public void StartSpeedBoost(float speedMultiplier, float boostDuration)
+    {
+        if (activeBoost != null)
+            StopCoroutine(activeBoost);
+
+        activeBoost = StartCoroutine(SpeedBoostCoroutine(speedMultiplier, boostDuration));
+    }
+
+    private IEnumerator SpeedBoostCoroutine(float speedMultiplier, float boostDuration)
+    {
+        Debug.Log("Speed boost started! Multiplier: " + speedMultiplier);
+        currentSpeed = moveSpeed * speedMultiplier;
+
+        yield return new WaitForSeconds(boostDuration);
+
+        currentSpeed = moveSpeed;
+        activeBoost = null;
+        Debug.Log("Speed boost ended.");
     }
 }
